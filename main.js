fc2e5bcb0cef10c4741917d6d2e0a39728d574ae
Greg Graham <greg@grahamtx.net>
2013-12-27 20:40:45 -0600

    Moving daleks
    Added the tardis and moving daleks to the game.

--- main.js
+++ main.js
@@ -5,15 +5,22 @@ var HSIZE = CELL_SIZE * COLUMNS;
 var VSIZE = CELL_SIZE * ROWS;
 
 var DOCTOR_FRAME = 0;
+var DALEK_FRAME = 1;
+var TARDIS_FRAME = 2;
 var SPEED = 3;
+var NUM_DALEKS = 15;
 
 enchant();
+
+function randInt(limit) {
+	return Math.floor(Math.random() * limit);
+}
 
 function CellLoc(cellX, cellY) {
 	this.cellX = cellX;
 	this.cellY = cellY;
 	
-	this.bounds = function() {
+	this.normalize = function() {
 		if (this.cellX < 0) this.cellX = 0;
 		else if (this.cellX >= COLUMNS) this.cellX = COLUMNS - 1;
 		if (this.cellY < 0) this.cellY = 0;
@@ -22,45 +29,173 @@ function CellLoc(cellX, cellY) {
 		this.pixelY = this.cellY * CELL_SIZE;
 	};
 	
-	this.bounds();
+	this.normalize();
 	
 	this.moveN = function() {
 		this.cellY--;
-		this.bounds();
+		this.normalize();
 	};
 	this.moveNE = function() {
 		this.cellY--;
 		this.cellX++;
-		this.bounds();
+		this.normalize();
 	};
 	this.moveE = function() {
 		this.cellX++;
-		this.bounds();
+		this.normalize();
 	};
 	this.moveSE = function() {
 		this.cellY++;
 		this.cellX++;
-		this.bounds();
+		this.normalize();
 	};
 	this.moveS = function() {
 		this.cellY++;
-		this.bounds();
+		this.normalize();
 	};
 	this.moveSW = function() {
 		this.cellY++;
 		this.cellX--;
-		this.bounds();
+		this.normalize();
 	};
 	this.moveW = function() {
 		this.cellX--;
-		this.bounds();
+		this.normalize();
 	};
 	this.moveNW = function() {
 		this.cellY--;
 		this.cellX--;
-		this.bounds();
+		this.normalize();
 	};
+	
+	this.moveTowardsPixel = function(destX, destY) {
+		var deltaX = destX - (this.pixelX + CELL_SIZE / 2);
+		var deltaY = destY - (this.pixelY + CELL_SIZE / 2);
+		var angle = Math.atan2(deltaY, deltaX) * 180 / Math.PI;
+		console.log(angle);
+		
+		if (angle < -150)
+			this.moveW();
+		else if (angle < -120)
+			this.moveNW();
+		else if (angle < -60)
+			this.moveN();
+		else if (angle < -30)
+			this.moveNE();
+		else if (angle < 30)
+			this.moveE();
+		else if (angle < 60)
+			this.moveSE();
+		else if (angle < 120)
+			this.moveS();
+		else if (angle < 150)
+			this.moveSW();
+		else
+			this.moveW();
+	};
+	
+	this.moveTowardsCell = function(cellLoc) {
+		this.moveTowardsPixel(cellLoc.pixelX, cellLoc.pixelY);
+	}
+}
 
+function Doctor(cellLoc, image) {
+	this.cellLoc = cellLoc
+	this.sprite = new Sprite(CELL_SIZE, CELL_SIZE);
+	this.sprite.image = image;
+	this.sprite.x = this.cellLoc.pixelX;
+	this.sprite.y = this.cellLoc.pixelY;
+	this.sprite.frame = DOCTOR_FRAME;
+	
+	var s = this.sprite;
+	var cl = this.cellLoc;
+	this.sprite.addEventListener(Event.ENTER_FRAME, function() {
+		if (s.y > cl.pixelY) {
+			if (Math.abs(s.y - cl.pixelY) < SPEED) {
+				s.y = cl.pixelY;
+			} else {
+				s.y -= SPEED;
+			}
+		} else if (s.y < cl.pixelY) {
+			if (Math.abs(s.y - cl.pixelY) < SPEED) {
+				s.y = cl.pixelY;
+			} else {
+				s.y += SPEED;
+			}
+		}
+		if (s.x > cl.pixelX) {
+			if (Math.abs(s.x - cl.pixelX) < SPEED) {
+				s.x = cl.pixelX;
+			} else {
+				s.x -= SPEED;
+			}
+		} else if (s.x < cl.pixelX) {
+			if (Math.abs(s.x - cl.pixelX) < SPEED) {
+				s.x = cl.pixelX;
+			} else {
+				s.x += SPEED;
+			}
+		}
+
+	});
+
+	this.moveTo = function(e) {
+		cl.moveTowardsPixel(e.x, e.y);
+	};
+}
+
+function Dalek(cellLoc, image) {
+	this.cellLoc = cellLoc;
+	this.sprite = new Sprite(CELL_SIZE, CELL_SIZE);
+	this.sprite.image = image;
+	this.sprite.x = this.cellLoc.pixelX;
+	this.sprite.y = this.cellLoc.pixelY;
+	this.sprite.frame = DALEK_FRAME;
+	
+	var s = this.sprite;
+	var cl = this.cellLoc;
+	this.sprite.addEventListener(Event.ENTER_FRAME, function() {
+		if (s.y > cl.pixelY) {
+			if (Math.abs(s.y - cl.pixelY) < SPEED) {
+				s.y = cl.pixelY;
+			} else {
+				s.y -= SPEED;
+			}
+		} else if (s.y < cl.pixelY) {
+			if (Math.abs(s.y - cl.pixelY) < SPEED) {
+				s.y = cl.pixelY;
+			} else {
+				s.y += SPEED;
+			}
+		}
+		if (s.x > cl.pixelX) {
+			if (Math.abs(s.x - cl.pixelX) < SPEED) {
+				s.x = cl.pixelX;
+			} else {
+				s.x -= SPEED;
+			}
+		} else if (s.x < cl.pixelX) {
+			if (Math.abs(s.x - cl.pixelX) < SPEED) {
+				s.x = cl.pixelX;
+			} else {
+				s.x += SPEED;
+			}
+		}
+
+	});
+	
+	this.moveTo = function(cellLoc) {
+		this.cellLoc.moveTowardsCell(cellLoc);
+	}
+}
+
+function Tardis(cellLoc, image) {
+	this.cellLoc = cellLoc;
+	this.sprite = new Sprite(CELL_SIZE, CELL_SIZE);
+	this.sprite.image = image;
+	this.sprite.x = this.cellLoc.pixelX;
+	this.sprite.y = this.cellLoc.pixelY;
+	this.sprite.frame = TARDIS_FRAME;
 }
 
 window.onload = function() {
@@ -79,79 +214,37 @@ window.onload = function() {
                     // maptip: the preloaded image asset used as the source image
                     // 0, 0: coordinates of upper left corner of the source clipping
                     // CELL_SIZE, CELL_SIZE: width and height of the source clipping
-                    // i, j: coorinates of upper left corner of the destination
+                    // i, j: coordinates of upper left corner of the destination
                     // CELL_SIZE, CELL_SIZE: width and height of the destination
             }
         }
         
         bg.image = image;
         game.rootScene.addChild(bg);
+		
+		var doctor = new Doctor(new CellLoc(COLUMNS - 1, randInt(ROWS)),
+								game.assets['daleks.png']);
+		game.rootScene.addChild(doctor.sprite);
 
-        var doctor = new Sprite(CELL_SIZE, CELL_SIZE);
-        doctor.image = game.assets['daleks.png'];
-		doctor.cellLoc = new CellLoc(Math.floor(COLUMNS / 2), Math.floor(ROWS / 2));
-        doctor.x = doctor.cellLoc.pixelX;
-        doctor.y = doctor.cellLoc.pixelY;
-        doctor.frame = DOCTOR_FRAME;
-        
-        game.rootScene.addChild(doctor);
-        doctor.addEventListener(Event.ENTER_FRAME, function() {
-            if (doctor.y > doctor.cellLoc.pixelY) {
-                if (Math.abs(doctor.y - doctor.cellLoc.pixelY) < SPEED) {
-                    doctor.y = doctor.cellLoc.pixelY;
-                } else {
-                    doctor.y -= SPEED;
-                }
-            } else if (doctor.y < doctor.cellLoc.pixelY) {
-                if (Math.abs(doctor.y - doctor.cellLoc.pixelY) < SPEED) {
-                    doctor.y = doctor.cellLoc.pixelY;
-                } else {
-                    doctor.y += SPEED;
-                }
-            }
-            if (doctor.x > doctor.cellLoc.pixelX) {
-                if (Math.abs(doctor.x - doctor.cellLoc.pixelX) < SPEED) {
-                    doctor.x = doctor.cellLoc.pixelX;
-                } else {
-                    doctor.x -= SPEED;
-                }
-            } else if (doctor.x < doctor.cellLoc.pixelX) {
-                if (Math.abs(doctor.x - doctor.cellLoc.pixelX) < SPEED) {
-                    doctor.x = doctor.cellLoc.pixelX;
-                } else {
-                    doctor.x += SPEED;
-                }
-            }
-
-        });
-        
-		function touchResponse(e) {
-            var deltaX = e.x - (doctor.cellLoc.pixelX + CELL_SIZE / 2);
-            var deltaY = e.y - (doctor.cellLoc.pixelY + CELL_SIZE / 2);
-			var angle = Math.atan2(deltaY, deltaX) * 180 / Math.PI;
-			console.log(angle);
-			
-			if (angle < -150)
-				doctor.cellLoc.moveW();
-			else if (angle < -120)
-				doctor.cellLoc.moveNW();
-			else if (angle < -60)
-				doctor.cellLoc.moveN();
-			else if (angle < -30)
-				doctor.cellLoc.moveNE();
-			else if (angle < 30)
-				doctor.cellLoc.moveE();
-			else if (angle < 60)
-				doctor.cellLoc.moveSE();
-			else if (angle < 120)
-				doctor.cellLoc.moveS();
-			else if (angle < 150)
-				doctor.cellLoc.moveSW();
-			else
-				doctor.cellLoc.moveW();
+		var daleks = new Array(NUM_DALEKS);
+		for (var i = 0; i < NUM_DALEKS; i++) {
+			daleks[i] = new Dalek(new CellLoc(1 + randInt(COLUMNS - 3), randInt(ROWS)),
+								  game.assets['daleks.png']);
+			game.rootScene.addChild(daleks[i].sprite);
 		}
 		
-        bg.addEventListener(Event.TOUCH_START, touchResponse);
+		var tardis = new Tardis(new CellLoc(0, randInt(ROWS)),
+								game.assets['daleks.png']);
+		game.rootScene.addChild(tardis.sprite);
+
+		
+        bg.addEventListener(Event.TOUCH_START, function(e) {
+		    doctor.moveTo(e);
+			for (var i = 0; i < NUM_DALEKS; i++) {
+				daleks[i].moveTo(doctor.cellLoc);
+			}
+		});
+
     };
     game.start();
 };
